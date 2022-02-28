d04f9b81a87231e1eedd94c7f5787b9e7963a67e
punkason <punkason@gmail.com>
2022-02-28 21:48:40 +0000

    Add swagger

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -2,6 +2,14 @@ package com.techreturners.bookmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BookmanagerApplication {
@@ -10,4 +18,21 @@ public class BookmanagerApplication {
 		SpringApplication.run(BookmanagerApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.OAS_30)
+		.select()
+		.paths(PathSelectors.any())
+		.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+		.build()
+		.apiInfo(apiInformation());
+		 }
+
+	private ApiInfo apiInformation() {
+		return new ApiInfoBuilder()
+		.title(" Minimalist Book Manager API")
+		.description("Need a helping hand to manage your books? This is the API for you! ")
+		.version("v1")
+		.build();
+		}
 }
